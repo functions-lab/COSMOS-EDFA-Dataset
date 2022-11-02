e9b79815fe20f8dca24f313a4334bd2fdff27974
zehao <zehao.w@duke.edu>
2022-11-02 16:59:55 -0400

    change md file

---
+++ code/examples.py
@@ -0,0 +1,33 @@
+# example code for visualization the EDFA gain spectrum dataset
+from libs.edfa_examples import *
+
+####################
+# MAIN (examples)
+####################
+def run_examples(option):
+    if option == 1:
+        edfaType,gain,channelType,roadmName = "booster","18dB","random","rdm1-co1"
+        plot_one_json_file_gain_spectrum(edfaType,gain,channelType,roadmName)
+
+    elif option == 2:
+        # get from website introduction or README / 
+        edfaType,gain,channelType,roadmName = "preamp","18dB","fix","rdm1-co1"
+        # edfaType,gain,channelType,roadmName,subChannelName,spectrumName
+        subChannelName,spectrumName = "fully_loaded_channel_wdm","roadm_dut_preamp_input_power_spectra"
+        plot_json_one_element(edfaType,gain,channelType,roadmName,subChannelName,spectrumName)
+
+    elif option == 3:
+        channelTypes = ['fix', 'random', 'extraRandom', 'extraLow']
+        edfaTypes = ["booster"] # ["booster","preamp"]
+        roadmNames = ['rdm1-co1'] # ['rdm1-co1', 'rdm2-co1', 'rdm3-co1', 'rdm4-co1',
+                                # 'rdm5-co1', 'rdm6-co1', 'rdm1-lg1', 'rdm2-lg1']
+        gainLists = ["15dB","18dB","21dB"]
+        generate_ML_features(edfaTypes,gainLists,roadmNames,channelTypes)
+        # check the generated csv files at "misc/ML_features" folder
+
+    else:
+        # not implemented but feel free to implemented by yourself : )
+        raise Exception("not implemented.")
+
+# run the examples
+run_examples(option=2)
---
+++ code/libs/edfaBasicLib.py
@@ -0,0 +1,58 @@
+ ###############################################
+# all the external libs for EDFA postprocessing 
+###############################################
+
+# data
+import numpy as np
+import pandas as pd
+import scipy.stats as stats
+import json,copy
+from collections import defaultdict
+import statistics
+
+# MISC
+import math,os,shutil,fnmatch
+import datetime,pprint
+import matplotlib.pyplot as plt
+
+####################
+# DEFAULT CONFIGURATIONS
+####################
+
+# basic configuration
+colorLabel = ['red','green','blue','black','yellow','olive','brown','purple']
+
+CHANNEL_TYPES = ["fix","extraLow","random","extraRandom"]
+EDFA_TYPES_TO_GAINS ={
+    "booster":["15dB","18dB","21dB"],
+    "preamp":["15dB","18dB","21dB","24dB","27dB"]
+}
+pp = pprint.PrettyPrinter(indent=4)
+
+####################
+# HELPER FUNCTIONS
+####################
+
+# add files paths both works for linux and windows
+# input: [base_folder,subfolder1,subfolder2,subfolder3,target_file]
+# output:"base_folder/subfolder1/.../target_file"
+def get_path_to_file(folderList):
+    return os.path.join(*folderList)
+
+def getJsonData(fileName):
+    with open(fileName, "r") as read_file:
+        data = json.load(read_file)
+    return data['measurement_data']    
+
+def matchFile(pattern, foler):
+    # match one file in the folder
+    # example usage:
+    # result = matchFile('*rdm1-co1*.json', '.../15dB/extraRandom/')
+    # result is the full path 
+    for file in os.listdir(foler):
+        if fnmatch.fnmatch(file, pattern):
+            return os.path.join(foler, file)
+
+
+# other configuration
+DATASET_PATH = get_path_to_file(["..","dataset"])
